e248e34c58ccf83c7f636035a88f7c502b30ebb3
BKajganich6321 <bkajganich6321@conestogac.on.ca>
2026-07-22 14:35:25 -0400

    Create FreshIDController.cs
    create class

---
+++ FreshIDController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2025
+{
+    internal class FreshIDController
+    {
+        public (double start, double end)[] Intervals { get; set; }
+
+        public double[] FreshItems { get; set; }
+
+        public FreshIDController (string path)
+        {
+            ID = id;
+            idString = name; 
+        }
+
+    }
+}
